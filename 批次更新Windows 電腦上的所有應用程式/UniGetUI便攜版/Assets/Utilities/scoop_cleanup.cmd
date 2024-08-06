5dce489b215ac7ba117c6be34726ccb654e0b394
yu-rocky <yurocky817@gmail.com>
2024-08-07 01:37:55 +0800

    裝機

---
+++ 批次更新Windows 電腦上的所有應用程式/UniGetUI便攜版/Assets/Utilities/scoop_cleanup.cmd
@@ -0,0 +1,12 @@
+@echo off
+echo This script will clean the Scoop cache and run the cleanup command.
+echo|set/p="Press <ENTER> to continue or CLOSE this window to abort this process"&runas/u: "">NUL
+cls
+echo Cleaning Scoop cache...
+call scoop cleanup --all
+call scoop cleanup --all --global
+call scoop cache rm --all
+call scoop cache rm --all --global
+echo Done!
+pause
+exit
